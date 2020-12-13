89ecb4bb4437f49bef54a356f8b8819ea341697b
shubhraneel <shubhraneelpal@gmail.com>
2020-12-13 12:17:55 +0530

    Remove graphviz

--- iristree.py
+++ iristree.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import csv
-import graphviz
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
